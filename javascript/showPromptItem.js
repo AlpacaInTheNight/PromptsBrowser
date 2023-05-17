5f2679f924931f1c4fe764be1548c265a50b9641
AlpacaInTheNight <greenwoods11a@gmail.com>
2023-05-17 13:12:47 +0300

    Prompt item styling update

--- javascript/showPromptItem.js
+++ javascript/showPromptItem.js
@@ -8,6 +8,7 @@ PromptsBrowser.showPromptItem = (promptItem, options = {}) => {
 	const imageSrc = url || PromptsBrowser.utils.getPromptPreviewURL(id, undefined);
 
 	const promptElement = document.createElement("div");
+	const weightContainer = document.createElement("div");
 	promptElement.className = "PBE_promptElement PBE_currentElement";
 	promptElement.style.backgroundImage = imageSrc;
 	promptElement.dataset.prompt = id;
@@ -15,33 +16,45 @@ PromptsBrowser.showPromptItem = (promptItem, options = {}) => {
 	promptElement.draggable = "true";
 	if(isExternalNetwork) promptElement.classList.add("PBE_externalNetwork");
 	if(isShadowed) promptElement.classList.add("PBE_shadowedElement");
-	if(weight !== DEFAULT_PROMPT_WEIGHT) promptElement.innerText += " " + weight;
 
-	if(weight <= 0.8 && weight > 0.6) {
+	if(weight !== DEFAULT_PROMPT_WEIGHT) {
+		weightContainer.className = "PBE_promptElementWeight";
+		weightContainer.innerText = weight;
+
+		promptElement.appendChild(weightContainer);
+	}
+
+	if(weight <= 1 && weight > 0.6) {
 		promptElement.style.transform = "scale(0.9)";
 		promptElement.style.zIndex = 3;
+		weightContainer.style.color = "green";
 
 	} else if(weight <= 0.6 && weight > 0.4) {
 		promptElement.style.transform = "scale(0.8)";
 		promptElement.style.zIndex = 2;
+		weightContainer.style.color = "blue";
 		
 	} else if(weight <= 0.4) {
 		promptElement.style.transform = "scale(0.7)";
 		promptElement.style.zIndex = 1;
+		weightContainer.style.color = "purple";
 
 	}
 
 	if(weight > 1 && weight <= 1.2) {
 		promptElement.style.transform = "scale(1.1)";
 		promptElement.style.zIndex = 4;
+		weightContainer.style.color = "orange";
 
 	} else if(weight > 1.2 && weight <= 1.3) {
 		promptElement.style.transform = "scale(1.2)";
 		promptElement.style.zIndex = 5;
+		weightContainer.style.color = "orangered";
 
 	} else if(weight > 1.3) {
 		promptElement.style.transform = "scale(1.3)";
 		promptElement.style.zIndex = 6;
+		weightContainer.style.color = "red";
 	}
 
 	if(!noSplash) {
@@ -49,7 +62,13 @@ PromptsBrowser.showPromptItem = (promptItem, options = {}) => {
 		splashElement.className = "PBE_promptElementSplash PBE_currentElement";
 		splashElement.style.backgroundImage = imageSrc;
 		splashElement.innerText = id;
-		if(weight !== DEFAULT_PROMPT_WEIGHT) splashElement.innerText += " " + weight;
+
+		if(weight !== DEFAULT_PROMPT_WEIGHT) {
+			/* weightContainer.className = "PBE_promptElementWeight";
+			weightContainer.innerText = weight; */
+	
+			splashElement.appendChild(weightContainer.cloneNode(true));
+		}
 
 		promptElement.appendChild(splashElement);
 
