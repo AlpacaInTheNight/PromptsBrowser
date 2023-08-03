6ceccd7c0a4a61453be38246d671460683f845c0
AlpacaInTheNight <greenwoods11a@gmail.com>
2023-08-03 21:31:31 +0300

    Autocomplete bugfix

--- javascript/synchroniseCurrentPrompts.js
+++ javascript/synchroniseCurrentPrompts.js
@@ -4,7 +4,7 @@ if(!window.PromptsBrowser) window.PromptsBrowser = {};
 /**
  * Synchronises text content of the textarea with the array of active prompts used by the extension.
  */
-PromptsBrowser.synchroniseCurrentPrompts = () => {
+PromptsBrowser.synchroniseCurrentPrompts = (noTextAreaUpdate = true) => {
 	const {normalizePrompt} = window.PromptsBrowser;
 	const {DEFAULT_PROMPT_WEIGHT, PROMPT_WEIGHT_FACTOR} = PromptsBrowser.params;
 	const {state} = PromptsBrowser;
@@ -12,6 +12,8 @@ PromptsBrowser.synchroniseCurrentPrompts = () => {
 	if(!textArea) return;
 	let activePrompts = PromptsBrowser.getCurrentPrompts();
 	let value = textArea.value;
+    console.log("synchronise: activePrompts - ", activePrompts);
+    console.log("synchronise: value - ", value);
 
 	//trying to fix LORAs/Hypernetworks added without a preceding comma
 	value = value.replace(/([^,])\ </g, "$1,\ <");
@@ -46,7 +48,8 @@ PromptsBrowser.synchroniseCurrentPrompts = () => {
 	}
 
 	activePrompts = newPBE_currentPrompts;
+    console.log("synchronise: NEW activePrompts - ", activePrompts);
 
 	PromptsBrowser.setCurrentPrompts(activePrompts);
-	PromptsBrowser.currentPrompts.update(true);
+	PromptsBrowser.currentPrompts.update(noTextAreaUpdate);
 }
