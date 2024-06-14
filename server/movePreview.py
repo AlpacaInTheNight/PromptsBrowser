c0c1137077b6c80d66f52501bf28ee2bde6d55e2
AlpacaInTheNight <greenwoods11a@gmail.com>
2024-06-14 19:32:38 +0300

    move/copy/delete support for model previews

--- server/movePreview.py
+++ server/movePreview.py
@@ -8,6 +8,54 @@ from .utils import makeFileNameSafe
 from .utils import getCollectionsDir
 from .utils import emitMessage
 
+def moveModelPreviews(postJSON):
+    item = postJSON.item
+    itemFrom = postJSON.movefrom
+    itemTo = postJSON.to
+    itemType = postJSON.type
+
+    if not item or not itemFrom or not itemTo or not itemType: return "failed"
+
+    collDir = getCollectionsDir()
+
+    pathPromptsCatalogue    = join(collDir, constant.PROMPTS_DIR)
+    pathToPreviewDir        = join(pathPromptsCatalogue, itemFrom, "preview")
+    safeFileName            = makeFileNameSafe(item)
+
+    colModelsDirs = os.listdir(pathToPreviewDir)
+    for colModelDirName in colModelsDirs:
+        if not isdir(join(pathToPreviewDir, colModelDirName)): continue
+
+        pathToPreviewDir        = join(pathPromptsCatalogue, itemFrom, "preview", colModelDirName)
+        pathSaveDest            = join(pathPromptsCatalogue, itemTo, "preview", colModelDirName)
+        os.makedirs(pathSaveDest, exist_ok=True)
+
+        extension = ".png"
+
+        imageFileNames = [filename for filename in os.listdir(pathToPreviewDir) if filename.endswith('.jpg') or filename.endswith('.png')]
+        if safeFileName + ".png" in imageFileNames: extension = ".png"
+        elif safeFileName + ".jpg" in imageFileNames: extension = ".jpg"
+        else: continue
+
+        sourcePath = join(pathToPreviewDir, safeFileName + extension)
+        savePath = join(pathSaveDest, safeFileName + extension)
+
+        if not isfile(sourcePath): continue
+
+        if itemType == "copy":
+            shutil.copy(sourcePath, savePath)
+            emitMessage(f'preview image: copied prompt preview "{item}" from "{itemFrom}" to "{itemTo}" for model "{colModelDirName}"')
+
+        elif itemType == "move":
+            shutil.move(sourcePath, savePath)
+            emitMessage(f'preview image: moved prompt preview "{item}" from "{itemFrom}" to "{itemTo}" for model "{colModelDirName}"')
+        
+        elif itemType == "delete":
+            os.remove(sourcePath)
+            emitMessage(f'preview image: deleted prompt preview "{item}" from "{itemFrom}" for model "{colModelDirName}"')
+        
+        return "ok"
+
 def movePreview(postJSON):
     item = postJSON.item
     itemFrom = postJSON.movefrom
@@ -15,6 +63,8 @@ def movePreview(postJSON):
     itemType = postJSON.type
 
     if not item or not itemFrom or not itemTo or not itemType: return "failed"
+
+    moveModelPreviews(postJSON)
 
     collDir = getCollectionsDir()
 
